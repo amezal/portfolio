dda414f4466ad12bebdc0edc273bbc8c85b8a6a9
Armando <amezal01@hotmail.com>
2022-04-23 14:07:36 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Armando Meza's Portfolio</h1>
+    </div>
+  )
+}
+
+export default App
