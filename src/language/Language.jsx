99a7081b49ed93cf855f1c926dbcf9f9dae05278
Armando <amezal01@hotmail.com>
2022-05-09 19:39:48 -0600

    language provider

---
+++ src/language/Language.jsx
@@ -0,0 +1,48 @@
+import React, { useState, createContext, useContext } from 'react';
+
+const dictionaryList = {
+  en: {
+    nav: {
+      work: 'Work',
+      resume: 'Resume',
+      contact: 'Contact',
+    },
+    home: 'Hello'
+  },
+  es: {
+    nav: {
+      work: 'Proyectos',
+      resume: 'Currículum',
+      contact: 'Contacto',
+    },
+  },
+}
+
+const LanguageContext = createContext({
+  userLanguage: 'en',
+  dictionary: dictionaryList.en,
+}
+);
+
+export const LanguageProvider = ({ children }) => {
+  const defaultLanguage = window.localStorage.getItem('amezal-lang') || navigator.language.split('-')[0];
+
+  const [userLanguage, setUserLanguage] = useState(defaultLanguage || 'en');
+
+  const provider = {
+    userLanguage,
+    dictionary: dictionaryList[userLanguage],
+    changeLanguage: selected => {
+      setUserLanguage(selected);
+      window.localStorage.setItem('amezal-lang', selected);
+    }
+  }
+
+  return (
+    <LanguageContext.Provider value={provider}>
+      {children}
+    </LanguageContext.Provider>
+  )
+}
+
+export const useLanguage = () => useContext(LanguageContext);
